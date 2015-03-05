7688da6044e0993537202107d14a66a4b14d5c64
Cary FitzHugh <cary.fitzhugh@gmail.com>
2015-03-05 09:16:34 -0500

    woot, rewinding!

--- js/gameboard.js
+++ js/gameboard.js
@@ -3,12 +3,11 @@ window.gameboard = new Ractive({
   template: "#gameboard_template",
   data: {
     player: { top: 50, left: 50},
-
-    forks: [
-      {top: 1, left: 5},
-      {top: 30, left: 45},
-      {top: 70, left: 25},
-      {top: 100, left: 85}
-    ]
+    forks: [],
+    internal: {
+      game_mode: 'play',
+      total_frames: 0,
+      rewind_frame: 0
+    }
   }
 });
--- js/gameloop.js
+++ js/gameloop.js
@@ -1,34 +1,101 @@
 var player_delta = {top:0, left: 0};
-var move_rate = 0.5;
+var move_rate = 1;
+var game_history = [];
 
-Mousetrap.bind('left', function() { player_delta.left -= move_rate; return false;});
-Mousetrap.bind('right', function() { player_delta.left += move_rate; return false;});
-Mousetrap.bind('up', function() { player_delta.top -= move_rate; return false;});
-Mousetrap.bind('down', function() { player_delta.top += move_rate; return false;});
+// How far "back" in history are we operating?
+var history_index = function (v) {
+  if (!_.isUndefined(v)) {
+    window.gameboard.set('internal.rewind_frame', v);
+  }
+  return window.gameboard.get('internal.rewind_frame');
+};
+
+var set_game_state = function (i) {
+  var state = game_history[i];
+  _.each( state, function (v, name) {
+    window.gameboard.set(name, v);
+  });
+};
+
+var stop_game = function () {
+  window.gameboard.set('internal.game_mode' ,'rewind')
+  history_index(game_history.length)
+};
+
+var resume_game =  function() {
+  window.gameboard.set('internal.game_mode' ,'play')
+  // Delete any future history from this point onward.
+  game_history = _.slice(game_history, 0, history_index());
+  history_index(0);
+};
 
 // Moves the player
 var move_player = function (player) {
   var current = window.gameboard.get('player');
   player.top += player_delta.top;
   player.left += player_delta.left;
-
-  player_delta = {top:0, left: 0};
   return player;
 };
 
-
 setInterval(function () {
-  window.gameboard.set('player', move_player(window.gameboard.get('player') ));
+  var running = window.gameboard.get('internal.game_mode') === 'play';
 
-  var forks = window.gameboard.get('forks');
+  if (running) {
+    //
+    // WE ARE IN RUNNING MODE!
+    //
+    // BE SAFE!
+    history_index(0);
 
-  // Set the vars
-  window.gameboard.set('forks',
-    _.map(forks, function (fork) {
-      fork.left = fork.left + _.random(2) - 1;
-      fork.top = fork.top + _.random(2) - 1;
-      return fork;
-    })
-  );
+    // Move the player
+    window.gameboard.set('player', move_player(window.gameboard.get('player') ));
+
+    // Save history
+    var state = _.cloneDeep(window.gameboard.get());
+    delete state.internal;
+    game_history.push(state);
+    window.gameboard.set('internal.total_frames', game_history.length);
+
+  } else {
+
+    // IF WE ARE IN "META-TIME MODE"
+    // Where you can go fwd / back in time
+    //
+    if (player_delta.left < 0) {
+      history_index(history_index() - 1);
+
+      if (history_index() < 0) {
+        history_index(0);
+      }
+
+      // Go "back" in time one step
+      set_game_state(history_index());
+
+
+    } else if (player_delta.left > 0) {
+
+      history_index(history_index() + 1);
+
+      // Go forward (or resume)
+      if (history_index() >= game_history.length) {
+        resume_game();
+      } else {
+        set_game_state(history_index());
+      }
+    }
+  }
+
+  // Reset player inputs
+  player_delta = {top:0, left: 0};
+
 }
 , 100);
+
+
+Mousetrap.bind('left', function() { player_delta.left -= move_rate; return false;});
+Mousetrap.bind('right', function() { player_delta.left += move_rate; return false;});
+Mousetrap.bind('up', function() { player_delta.top -= move_rate; return false;});
+Mousetrap.bind('down', function() { player_delta.top += move_rate; return false;});
+
+Mousetrap.bind('esc', function() { stop_game();});
+Mousetrap.bind('space', function() { resume_game();});
