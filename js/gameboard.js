217faad9f9c016cb41166144af8cdfd27b003395
Cary FitzHugh <cary.fitzhugh@gmail.com>
2015-03-05 08:23:49 -0500

    updated

--- js/gameboard.js
+++ js/gameboard.js
@@ -2,11 +2,13 @@ window.gameboard = new Ractive({
   el: $("#gameboard"),
   template: "#gameboard_template",
   data: {
+    player: { top: 50, left: 50},
+
     forks: [
       {top: 1, left: 5},
       {top: 30, left: 45},
       {top: 70, left: 25},
       {top: 100, left: 85}
-   ]
+    ]
   }
 });
--- js/gameloop.js
+++ js/gameloop.js
@@ -1,4 +1,25 @@
+var player_delta = {top:0, left: 0};
+var move_rate = 0.5;
+
+Mousetrap.bind('left', function() { player_delta.left -= move_rate; return false;});
+Mousetrap.bind('right', function() { player_delta.left += move_rate; return false;});
+Mousetrap.bind('up', function() { player_delta.top -= move_rate; return false;});
+Mousetrap.bind('down', function() { player_delta.top += move_rate; return false;});
+
+// Moves the player
+var move_player = function (player) {
+  var current = window.gameboard.get('player');
+  player.top += player_delta.top;
+  player.left += player_delta.left;
+
+  player_delta = {top:0, left: 0};
+  return player;
+};
+
+
 setInterval(function () {
+  window.gameboard.set('player', move_player(window.gameboard.get('player') ));
+
   var forks = window.gameboard.get('forks');
 
   // Set the vars
@@ -10,4 +31,4 @@ setInterval(function () {
     })
   );
 }
-, 1000);
+, 100);
