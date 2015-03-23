8a38ffc1f9a7a9b17ee33b951e1fdb253158c186
Cary FitzHugh <cary.fitzhugh@gmail.com>
2015-03-23 16:20:33 -0400

    update so that things repel / attract!

--- fork-unity/Assets/Scripts/Magnetizable.js
+++ fork-unity/Assets/Scripts/Magnetizable.js
@@ -2,6 +2,15 @@
 enum MagPolarity{ None, Positive, Negative};
 var polarity : MagPolarity = MagPolarity.None;
 public var glow : Light;
+public var attraction_range : float = 3.0;
+public var attraction_mask: LayerMask;
+
+// Attract!
+class MagnetismAttractionData extends System.ValueType
+{
+  var position : Vector3;
+  var polarity : MagPolarity;
+}
 
 function Start () {
 };
@@ -9,6 +18,41 @@ function Start () {
 function set_polarity (new_polarity) {
 Debug.Log("Setting polarity");
   polarity = new_polarity; 
+};
+
+function magnetize_update (args : MagnetismAttractionData) {
+
+  if (polarity != MagPolarity.None) {
+    var force = (transform.position - args.position);
+    var sqrLen = force.sqrMagnitude;
+
+    if (sqrLen > 0.5) {
+
+      if (args.polarity == polarity) {
+        Debug.Log("attract!");
+	transform.LookAt(args.position);
+	gameObject.GetComponent.<Rigidbody>().AddForce((args.position - transform.position) * 10 * sqrLen, ForceMode.Acceleration);
+      } else {
+        Debug.Log("repel!");
+	transform.LookAt(args.position);
+	gameObject.GetComponent.<Rigidbody>().AddForce((transform.position - args.position) * 10 * sqrLen, ForceMode.Acceleration);
+      }
+    }
+  }
+};
+
+function FixedUpdate() {
+  if (polarity && polarity != MagPolarity.None) {
+    var attraction_data = new MagnetismAttractionData();
+    attraction_data.position = transform.position;
+    attraction_data.polarity = polarity;
+
+    var hitColliders = Physics.OverlapSphere(transform.position, attraction_range, attraction_mask);
+          	
+    for (var i = 0; i < hitColliders.Length; i++) {
+      hitColliders[i].gameObject.SendMessage('magnetize_update', attraction_data);
+    }
+  }
 };
 
 function Update () {
