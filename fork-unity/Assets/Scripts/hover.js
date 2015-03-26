ebac9c429aaa6a039a8605420f26681303108a09
Dave Corboy <dave@corboy.com>
2015-03-26 01:43:16 -0400

    Some hovering crates
    are hovering in the great hall

---
+++ fork-unity/Assets/Scripts/hover.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var hoverHeight : float = 1.6;
+var power : float = 5;
+var balance : float = 98;
+//var dampen : float = 1;
+
+private var rbody : Rigidbody;
+
+function Start () {
+  rbody = GetComponent.<Rigidbody>();
+}
+
+function Update () {
+}
+
+function FixedUpdate() {
+  var throttle = (hoverHeight - transform.position.y) / hoverHeight;  // calculated throttle value
+  var thrust = balance * (throttle * power + 1);
+  rbody.AddForce(Vector3.up * thrust);
+  //Debug.Log("Thrust: " + thrust);
+}
