89e7e5c5f6fbcb2b2e636d98417dd13e320d8a52
Dave Corboy <dave@corboy.com>
2015-03-28 21:20:11 -0400

    Factor scripts into hover.js and stabilize.js

---
+++ fork-unity/Assets/Scripts/Stabilize.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+// Small forces and large disturbances induce angular momentum in the object.
+// This script will stabilize an object so that it remains upright. Plus, vector math is fun.
+
+var stability : float = 1.0;
+var speed : float = 2.0;
+
+private var rbody : Rigidbody;
+
+function Start () {
+  rbody = GetComponent.<Rigidbody>();
+}
+
+function Update () {
+}
+
+function FixedUpdate() {
+  // this produces a quaternion representing the predicted angular change of the object, based on current angular velocity
+  var predicted_rotation = Quaternion.AngleAxis(rbody.angularVelocity.magnitude * Mathf.Rad2Deg * stability / speed, rbody.angularVelocity);
+  // using this rotation, we predict the rotation of the up vector. We do not want this rotation.
+  var predicted_up = predicted_rotation * transform.up;
+  // so we apply torque along an axis perpendicular to the plane containing the desired and predicted up vectors
+  var torque_vector = Vector3.Cross(predicted_up, Vector3.up);
+  rbody.AddTorque(torque_vector * speed * speed);
+}
--- fork-unity/Assets/Scripts/hover.js
+++ fork-unity/Assets/Scripts/hover.js
@@ -1,27 +1,37 @@
 ﻿#pragma strict
 
-var hoverHeight : float = 1.6;
-var power : float = 1;
+// hover will float an object at either large or very small distances above the ground
+// For some reason, the small force adjustments may require the stabilization script at very small hover distances.
+
 var balance : float = 98;
+var hoverHeight : float = .01;  // with no spin, this designates the bottom of the object -- with spin, the center
+var sensitivity : float = .1;   // this can be larger for higher hover distances
 var spin : boolean = false;
-var spinPower : float = 1;
-//var dampen : float = 1;
+var spinPower : float = .2;
 
 private var rbody : Rigidbody;
+private var target_height : float;
 
 function Start () {
   rbody = GetComponent.<Rigidbody>();
+  target_height = GetComponent.<Renderer>().bounds.extents.y + hoverHeight;
 }
 
 function Update () {
 }
 
 function FixedUpdate() {
-  var throttle = (hoverHeight - transform.position.y) / hoverHeight;  // calculated throttle value
-  var thrust = balance * (throttle * power + 1);
+  var current_height : float;
+  if (spin) {
+    current_height = transform.position.y;  // find the bottom of the object
+  } else {
+    current_height = transform.position.y; //GetComponent.<Renderer>().bounds.min.y;  // find the bottom of the object
+  }
+  var throttle = (target_height - current_height) / target_height;  // calculated throttle value normalized
+  var thrust = (throttle * (sensitivity * balance)) + balance;
 
   rbody.AddForce(Vector3.up * thrust);
-  //Debug.Log("Thrust: " + thrust);
+  //Debug.Log("Target hgt: " + (target_height - bounds.min.y) + " Throttle: " + throttle + " Thrust: " + thrust);
 
   if (spin) {
     //spin_vector += (Vector3.right * spinPower);
