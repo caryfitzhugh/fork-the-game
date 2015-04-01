34b80833761fdf8342c90002b1c55d744f9f0a5c
Cary FitzHugh <cary.fitzhugh@gmail.com>
2015-04-01 09:48:23 -0400

    small changes so we are only torquing to match salme colored crates

--- fork-unity/Assets/Scripts/CF_MagnetizedCrates.js
+++ fork-unity/Assets/Scripts/CF_MagnetizedCrates.js
@@ -36,75 +36,75 @@ function process_nearby_objects (nearby_objects : Hashtable) {
       // This is our magnitude
       var magnitude = force_multiplier * nearby_objects.Count;
 
-      // This is an expensive calculation. You may want to just use the distance between the object centers.
-      var closestSurfacePoint1: Vector3  = GetComponent.<Collider>().ClosestPointOnBounds(target.transform.position);
-      var closestSurfacePoint2 : Vector3 = target.GetComponent.<Collider>().ClosestPointOnBounds(transform.position);
-      var surface_distance = Vector3.Distance(closestSurfacePoint1, closestSurfacePoint2);
-
-
-      if (surface_distance < 4) {
-
-        Debug.DrawLine(transform.position, target.transform.position, Color.green);
-        // this is the target line we want to match
-
-        var target_local = transform.InverseTransformPoint(target.transform.position);  // vector to target in local coordinates
-        //Debug.Log("my_loc_world: " + transform.position);
-        //Debug.Log("tgt_loc_world: " + target.transform.position);
-        //Debug.Log("tgt_local: " + target_local);
-
-        // Which face is closest to matching the desired direction?
-        var local_face_vector : Vector3;
-        if (Mathf.Abs(target_local.x) > Mathf.Abs(target_local.y) &&
-            Mathf.Abs(target_local.x) > Mathf.Abs(target_local.z)) {
-
-          if (target_local.x > 0) { local_face_vector = Vector3.right; }
-          else { local_face_vector = Vector3.left; }
-
-        } else if (Mathf.Abs(target_local.y) > Mathf.Abs(target_local.x) &&
-                   Mathf.Abs(target_local.y) > Mathf.Abs(target_local.z)) {
-          if (target_local.y > 0) { local_face_vector = Vector3.up; }
-          else { local_face_vector = Vector3.down; }
-
-        } else {
-          if (target_local.z > 0) { local_face_vector = Vector3.forward; }
-          else { local_face_vector = Vector3.back;}
-        }
-        //Debug.Log("lfv: " + local_face_vector);
-        var world_face_vector = transform.TransformVector(local_face_vector).normalized;
-        //Debug.Log("wfv (red): " + world_face_vector);
-        Debug.DrawLine(transform.position, transform.position + (3 * world_face_vector), Color.red);
-        // This is the face-normal that is closest to the target line, so we need to move this line to the green line
-        
-        var world_target_vector = (target.transform.position - transform.position).normalized;
-        //Debug.Log("wtv: " + world_target_vector);
-
-        var normal_axis = Vector3.Cross(world_target_vector, world_face_vector);
-        //Debug.Log("rotation axis (red): " + normal_axis);
-        Debug.DrawLine(transform.position, transform.position + (3 * normal_axis), Color.blue);
-        // This is the axis of rotation around which we must be rotated to match the desired orientation
-
-        // I refuse to believe that my vector is inverted so I invert it for AddTorque who is, of course, wrong. :)
-        var speed = -5; // it it hard to overcome static friction, but this torque works fine with a hover attached (though we'd have to dampen the torque, you'll see why)
-        //GetComponent.<Rigidbody>().AddTorque(normal_axis * speed);  // it also works on the floor at large values
-
-        // Make sure we can move it.
-        // http://docs.unity3d.com/ScriptReference/Rigidbody.SweepTest.html
-
-        // STEP 1) Adjust the rotation to match
-        // STEP 2) Move closer until the extents / bounds are within 0.1 (or something tiny).
-        // STEP 3) Attach a fixedJoint btwn you and the target
+      if (target_polarity == MagPolarity.None) {
+        // Do nothing -- the other guy is non-polarized
+      } else if (target_polarity != my_polarity.polarity) {
+        // You just push it away (opposite of the bottom equation)
+        GetComponent.<Rigidbody>().AddForce(-1.0 * vector * magnitude, ForceMode.Acceleration);
       } else {
+        // This is an expensive calculation. You may want to just use the distance between the object centers.
+        var closestSurfacePoint1: Vector3  = GetComponent.<Collider>().ClosestPointOnBounds(target.transform.position);
+        var closestSurfacePoint2 : Vector3 = target.GetComponent.<Collider>().ClosestPointOnBounds(transform.position);
+        var surface_distance = Vector3.Distance(closestSurfacePoint1, closestSurfacePoint2);
 
-        if (target_polarity == MagPolarity.None) {
-          // Do nothing -- the other guy is non-polarized
-        } else if (target_polarity != my_polarity.polarity) {
-          // You just push it away (opposite of the bottom equation)
-          GetComponent.<Rigidbody>().AddForce(-1.0 * vector * magnitude, ForceMode.Acceleration);
-        } else {
-          //target.transform.LookAt(transform.position);
+        if (surface_distance < 0.5) {
+          Debug.DrawLine(transform.position, target.transform.position, Color.green);
+          // this is the target line we want to match
+
+          var target_local = transform.InverseTransformPoint(target.transform.position);  // vector to target in local coordinates
+          //Debug.Log("my_loc_world: " + transform.position);
+          //Debug.Log("tgt_loc_world: " + target.transform.position);
+          //Debug.Log("tgt_local: " + target_local);
+
+          // Which face is closest to matching the desired direction?
+          var local_face_vector : Vector3;
+          if (Mathf.Abs(target_local.x) > Mathf.Abs(target_local.y) &&
+              Mathf.Abs(target_local.x) > Mathf.Abs(target_local.z)) {
+
+                if (target_local.x > 0) { local_face_vector = Vector3.right; }
+                else { local_face_vector = Vector3.left; }
+
+              } else if (Mathf.Abs(target_local.y) > Mathf.Abs(target_local.x) &&
+                  Mathf.Abs(target_local.y) > Mathf.Abs(target_local.z)) {
+                    if (target_local.y > 0) { local_face_vector = Vector3.up; }
+                    else { local_face_vector = Vector3.down; }
+
+                  } else {
+                    if (target_local.z > 0) { local_face_vector = Vector3.forward; }
+                    else { local_face_vector = Vector3.back;}
+                  }
+
+          //Debug.Log("lfv: " + local_face_vector);
+          var world_face_vector = transform.TransformVector(local_face_vector).normalized;
+          //Debug.Log("wfv (red): " + world_face_vector);
+          Debug.DrawLine(transform.position, transform.position + (3 * world_face_vector), Color.red);
+          // This is the face-normal that is closest to the target line, so we need to move this line to the green line
+
+          var world_target_vector = (target.transform.position - transform.position).normalized;
+          //Debug.Log("wtv: " + world_target_vector);
+
+          var normal_axis = Vector3.Cross(world_target_vector, world_face_vector);
+
+          //Debug.Log("rotation axis (red): " + normal_axis);
+          Debug.DrawLine(transform.position, transform.position + (3 * normal_axis), Color.blue);
+          // This is the axis of rotation around which we must be rotated to match the desired orientation
+
+          // I refuse to believe that my vector is inverted so I invert it for AddTorque who is, of course, wrong. :)
+          // it it hard to overcome static friction, but this torque works fine with a hover attached (though we'd have to dampen the torque, you'll see why)
+          var speed = -500;
+
+          // Only rotate around the Y ( to keep them on the ground?)
+          GetComponent.<Rigidbody>().AddTorque(normal_axis * speed);  // it also works on the floor at large values
+
+          // Make sure we can move it.
+          // http://docs.unity3d.com/ScriptReference/Rigidbody.SweepTest.html
+
+          // STEP 1) Adjust the rotation to match DONE
+          // STEP 2) Move closer until the extents / bounds are within 0.1 (or something tiny).
+          // STEP 3) Attach a fixedJoint btwn you and the target
+        } else  {
           GetComponent.<Rigidbody>().AddForce(vector * magnitude, ForceMode.Acceleration);
         }
-
       }
     }
   }
